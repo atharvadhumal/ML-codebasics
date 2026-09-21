98065e8b554b5d7082c4fa2ea3ca6437e83f35f8
Atharva Dhumal <atharvadhumal256@gmail.com>
2026-09-21 22:15:43 +0530

    lopps and elif

---
+++ python-basics/for_loop.py
@@ -0,0 +1,7 @@
+expense = [1200, 1400, 4000, 3700, 1300, 5400]
+total_expense = 0
+
+for expense in expense:
+    total_expense = total_expense + expense
+    
+print(total_expense)
---
+++ python-basics/if_condition.py
@@ -0,0 +1,7 @@
+n = input ("Enter a number")
+n = int(n)
+
+if n % 2 == 0:
+    print("Number is even")
+else:
+    print("Number is odd")
